4778f28f5417f22d12da1109c7f3924b65cbd868
AndreyRl <andrey.ordelino@sptech.school>
2022-03-12 17:16:08 -0300

    Adição do banco

---
+++ SPRINT_1.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE sprint_1;
+
+USE sprint_1;
+
+CREATE TABLE empresa (
+id_empresa INT PRIMARY KEY AUTO_INCREMENT,
+cnpj CHAR (14),
+nome_empresa VARCHAR (50),
+cidade VARCHAR (30),
+endereco VARCHAR (40),
+cep CHAR (8)
+);
+
+
+CREATE TABLE dado_sensor (
+id_sensor INT PRIMARY KEY AUTO_INCREMENT,
+tipo_sensor varchar(50),
+temperatura DOUBLE,
+dt_temp DATETIME
+);
+
